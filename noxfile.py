43b317ab768c990ed3ec65d40a1ee0f16d045abb
Anthonios Partheniou <partheniou@google.com>
2021-04-15 13:10:06 +0000

    Set scripts coverage to 80%

--- noxfile.py
+++ noxfile.py
@@ -90,7 +90,7 @@ def scripts(session):
         "--cov=scripts",
         "--cov-config=.coveragerc",
         "--cov-report=",
-        "--cov-fail-under=85",
+        "--cov-fail-under=80",
         "scripts",
         *session.posargs,
     )
